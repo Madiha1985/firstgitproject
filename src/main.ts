39a95080d5111158eee270ce0c595a6ac18dbd2b
Madiha <madihaiftikhar2013@gmail.com>
2024-12-09 11:51:25 +0100

    fist code

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 import './style.css'
+const firstName = document.createElement("p");
+firstName.innerHTML="Madiha";
+const div = document.getElementById("app");
+if (div){
+  div.appendChild(firstName);
+}
