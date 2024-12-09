5f411739c0fb91dfe8d11aa4efc48afe56f8fda3
AmandaBalk <Amanda_Balck@hotmail.com>
2024-12-09 11:51:45 +0100

    testar igen

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import './style.css'
-<<<<<<< HEAD
 
 const myName = ()=> {
     const name = "Amanda"
@@ -9,5 +8,3 @@ const myName = ()=> {
     }
   }
   myName();
-=======
->>>>>>> ca25d9db20b973d0e65480d6495194e362829f15
